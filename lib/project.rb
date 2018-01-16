ad34bec1f0b8ed2920a5f7daa4d8de548967c058
Sharmina Akhtar <sakht318@gmail.com>
2018-01-16 13:43:47 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,19 +1,16 @@
 class Project
-  attr_accessor :title, :backers
-#When a `Project` instance is initialized, it should be initialized with a `@backers`
-# variable set to an empty array. Instances of the `Project` class should have an `attr_accessor`
-#for backers so that projects can add backers to their list of backers and report on the
-#backers who support them.
 
-  def initialize (title)
+  attr_accessor :backer,
+
+  def initialize(title)
     @title = title
     @backers = []
   end
 
-  def add_backer(name)
-    @backers << name
+  def add_backer(backer)
+    @backer << name
     name.backed_projects << self
-    #@backed_projects << Project.new(title)
   end
+    
 
 end
